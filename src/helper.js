86afe873b88906445027635c563a7b60af099160
Todd Haskell <todd@craggypeak.com>
2021-11-19 20:47:47 -0800

    Troubleshooting JS

--- src/helper.js
+++ src/helper.js
@@ -2,7 +2,12 @@ function update () {
     console.log('in update');
     fetch(`https://v-thaskell-func.azurewebsites.net/api/HttpExample?name=foo`, { mode: 'no-cors'})
 	.then(response => response.text())
-	.then(setMessage);
+	.then(data => {
+	    console.log('Success:', data);
+	})
+	.catch((error) => {
+	    console.error('Error:', error);
+	});
 };
 
 function setMessage(message) {
